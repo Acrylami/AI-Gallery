338882d8f0a65fec8ae8e7a4fb9445dd3ea31ff7
david-osas <azamegbeosarumense@gmail.com>
2020-11-08 18:30:42 +0100

    Add footer component

--- client/image-ml/src/components/App.js
+++ client/image-ml/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import '../App.css';
 import Home from './Home';
+import Footer from './Footer';
 import Function from './Function';
 
 
 function App() {
   return (
+    <>
     <Switch>
       <Route exact path='/'>
         <Home />
@@ -18,7 +20,8 @@ function App() {
 
 
     </Switch>
-
+    <Footer />
+    </>
   );
 }
 
--- client/image-ml/src/components/Home.js
+++ client/image-ml/src/components/Home.js
@@ -3,7 +3,7 @@ import CardList from './CardList';
 import Heading from './Heading';
 function Home(){
   const head = 'Welcome to the image playground';
-  const subHead = 'were you can use machine learning to do lots of cool stuff with an image';
+  const subHead = 'where you can use machine learning to do lots of cool stuff with an image';
 
   return(
     <div>
