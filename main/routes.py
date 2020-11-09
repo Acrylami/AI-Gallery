ed1beba9d93146bff5a0a6a9fe8f73ebfd5331ee
Arlyn <Acrylami@gmail.com>
2020-11-09 14:55:13 +0000

    MemeSwapping works
    Will swap the uploaded face with bernie meme and return to the frontend

--- main/routes.py
+++ main/routes.py
@@ -5,7 +5,7 @@ from flask.helpers import flash
 #from PIL import Image
 #import PIL.ImageOps
 from werkzeug.utils import secure_filename
-from .FaceSwap import main, face_detection
+from .FaceSwap.main2 import run_swap as test
 
 #Will move this to config later, staying here for easier debug
 app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG", "JPG", "JPEG"] #Must double check with script we are using
@@ -31,13 +31,9 @@ def upload_image():
             filepath = 'main\\Images\\' + filename
             image.save(filepath)
             saved_path = 'Images\\' + filename
-            
-            #Process images
-            #test_processing(filepath) #Test inverting image
 
-            #Swaps faces with set meme and outputs as output.jpg
-            face_swap(filepath)
-            saved_path = 'main\\Images\\output.jpg'
+            #Swaps faces with set meme and outputs as result.jpg
+            saved_path = face_swap(filename)
             
             return send_file(saved_path, mimetype='image/jpg') #Must use different file path
     return 'test'
@@ -52,10 +48,17 @@ def test_processing(uploaded_image):
 '''
 
 def face_swap(uploaded_image):
-    meme = 'main\\Images\\OnceAgain.jpg'
-    output_filepath = 'main\\Images\\output.jpg'
-    main.main(meme, uploaded_image, output_filepath)
-    #command_string = "python main.py --src " + meme + " --dst " + uploaded_image + " --out " + output_filepath + " --correct_color"
-    #os.system(command_string)
+    image_path = os.getcwd()+ '\\main\\Images\\'
+    meme = 'OnceAgain.jpg' #Need better way to set meme
     
+    meme_filepath = image_path + meme
+    uploaded_filepath = image_path + uploaded_image
+    output_filepath = image_path + 'result.jpg'
+
+    test(uploaded_filepath, meme_filepath, output_filepath) #Runs the faceswapping scripts
+    #Delete uploaded files
+    if os.path.exists(uploaded_filepath):
+        os.remove(uploaded_filepath)
+    return output_filepath
+
     
