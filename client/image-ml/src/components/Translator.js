8d94362f644e531dadcb71a555bf82608ea7f74a
david-osas <azamegbeosarumense@gmail.com>
2020-11-10 10:33:52 +0100

    Add object detector

--- client/image-ml/src/components/Translator.js
+++ client/image-ml/src/components/Translator.js
@@ -25,7 +25,31 @@ function Translator(){
     }
     const model = await cocoSsd.load();
     const predictions = await model.detect(imgRef.current);
-    console.log(predictions);
+    if(predictions.length >== 1){
+      console.log('object detected');
+    }else{
+      console.log('no object detected');
+      return;
+    }
+    let nameSet = new Set();
+    for(let i = 0; i < predictions.length; i++){
+      nameSet.add(predictions[i].class);
+      if(nameSet.size > 1){
+        return;
+      }
+    }
+    const iterator = nameSet.values();
+    const name = iterator.next().value;
+
+    let options = {
+      "method": "POST",
+      "body": JSON.stringify({name}),
+    	"headers": {
+    		"content-type": "application/json",
+      },
+    };
+
+    let response = await fetch('/server/translate',options);
 
   }
   const uploadText = 'Detect objects';
